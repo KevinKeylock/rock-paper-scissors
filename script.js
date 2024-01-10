f502fd92efdd6e7a848d0836b992f645122fbb10
Kevin Keylock <kevinkeylock@hotmail.com>
2024-01-10 09:11:31 -0800

    Change order
    Player selection first

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function getPlayerSelection(){  
+  let playerSelection = prompt('Weapon of choice? Rock, Paper or Scissors').toLowerCase();  
+ 
+  return playerSelection;
+}
+
+const playerSelection = getPlayerSelection()
+console.log(playerSelection);
+
 function getComputerChoice (){
   //Square brackets for strings
     let answers = [
@@ -11,24 +20,7 @@ function getComputerChoice (){
       return computerSelection;
     }
 
-let computerSelection = getComputerChoice()
+const computerSelection = getComputerChoice()
 console.log(computerSelection)
 
-function getPlayerSelection(){  
-  let playerSelection = prompt('Weapon of choice? Rock, Paper or Scissors').toLowerCase();  
- 
-  return playerSelection;
-}
-
-let playerSelection = getPlayerSelection()
-console.log(playerSelection);
-
-function fight(playerSelection, computerSelection) {
-  if ("rock" < "scissors")
-    console.log("You Win")
-  else
-    console.log("You Lose")
-}
-
-fight()
 
