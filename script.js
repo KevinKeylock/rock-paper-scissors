8f6c43e5979736794c4da6194cb98b8577a1a7da
Kevin Keylock <kevinkeylock@hotmail.com>
2024-01-10 08:59:58 -0800

    Create variables for functions

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ function getComputerChoice (){
       
       let computerSelection = answers[Math.floor(Math.random() * answers.length)];
       
-      console.log(computerSelection);
+      return computerSelection;
     }
 
-getComputerChoice()
+let computerSelection = getComputerChoice()
+console.log(computerSelection)
 
-function getPlayerSelection(){
-let playerSelection = prompt('Weapon of choice? Rock, Paper or Scissors').toLowerCase();
-console.log(playerSelection);
+function getPlayerSelection(){  
+  let playerSelection = prompt('Weapon of choice? Rock, Paper or Scissors').toLowerCase();  
+ 
+  return playerSelection;
 }
 
-getPlayerSelection()
+let playerSelection = getPlayerSelection()
+console.log(playerSelection);
 
+function fight(playerSelection, computerSelection) {
+  if ("rock" < "scissors")
+    console.log("You Win")
+  else
+    console.log("You Lose")
+}
+
+fight()
 
